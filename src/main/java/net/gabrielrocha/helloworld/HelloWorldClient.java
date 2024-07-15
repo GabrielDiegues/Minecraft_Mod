92ab35c88753ddd1819c0ac5a61ae73b40a06ed7
Gabriel Rocha <gabrieldiegues12@gmail.com>
2024-07-15 13:51:34 -0300

    Minecraft source code

---
+++ src/main/java/net/gabrielrocha/helloworld/HelloWorldClient.java
@@ -0,0 +1,10 @@
+package net.gabrielrocha.helloworld;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HelloWorldClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        
+    }
+}
